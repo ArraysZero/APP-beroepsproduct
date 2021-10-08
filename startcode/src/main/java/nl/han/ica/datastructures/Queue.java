e25635687710ab745b5f7f9846b7e6fd5374e744
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-10-08 14:01:22 +0200

    implementatie queue

--- startcode/src/main/java/nl/han/ica/datastructures/Queue.java
+++ startcode/src/main/java/nl/han/ica/datastructures/Queue.java
@@ -1,4 +1,64 @@
 package nl.han.ica.datastructures;
 
-public class Queue {
+public class Queue<T> implements IHANQueue{
+	private Queue<T> next;
+	private T value;
+
+	public Queue(Queue<T> next, T value) {
+		this.next = next;
+		this.value = value;
+	}
+
+	public Queue(T value) {
+		this.value = value;
+	}
+
+	public Queue<T> getNext() {
+		return next;
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	@Override
+	public void clear() {
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public void enqueue(Object value) {
+		if (this.next != null){
+			this.next.enqueue(value);
+		} else {
+			this.next = new Queue(value);
+		}
+	}
+
+	@Override
+	public Object dequeue() {
+		T result = this.value;
+		this.value = this.next.getValue();
+		this.next = this.next.getNext();
+		return result;
+	}
+
+	@Override
+	public Object peek() {
+		return getValue();
+	}
+
+	@Override
+	public int getSize() {
+		if(this.next != null){
+			return this.next.getSize() + 1;
+		} else {
+			return 1;
+		}
+	}
 }
