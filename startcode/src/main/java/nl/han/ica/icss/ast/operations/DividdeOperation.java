d81f6f0f61a1666c0e5637d1111aa34b72a6e0ef
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-10-13 11:56:37 +0200

    een foutje

---
+++ startcode/src/main/java/nl/han/ica/icss/ast/operations/DividdeOperation.java
@@ -0,0 +1,10 @@
+package nl.han.ica.icss.ast.operations;
+
+import nl.han.ica.icss.ast.Operation;
+
+public class DividdeOperation extends Operation {
+	@Override
+	public String getNodeLabel() {
+		return "Divide";
+	}
+}
--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -1,5 +1,6 @@
 package nl.han.ica.icss.parser;
 
+import java.beans.Expression;
 import java.util.Stack;
 
 
@@ -13,6 +14,9 @@ import nl.han.ica.icss.ast.operations.SubtractOperation;
 import nl.han.ica.icss.ast.selectors.ClassSelector;
 import nl.han.ica.icss.ast.selectors.IdSelector;
 import nl.han.ica.icss.ast.selectors.TagSelector;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 /**
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
@@ -38,5 +42,245 @@ public class ASTListener extends ICSSBaseListener {
     public AST getAST() {
         return ast;
     }
-    
+
+	@Override
+	public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+		ASTNode stylesheet = new Stylesheet();
+		currentContainer.push(stylesheet);
+	}
+
+	@Override
+	public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
+		ast.setRoot((Stylesheet) currentContainer.pop());
+	}
+
+	@Override
+	public void enterStylerule(ICSSParser.StyleruleContext ctx) {
+		ASTNode styleRule = new Stylerule();
+		currentContainer.push(styleRule);
+	}
+
+	@Override
+	public void exitStylerule(ICSSParser.StyleruleContext ctx) {
+		super.exitStylerule(ctx);
+	}
+
+	@Override
+	public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
+		ASTNode declaration = new Declaration();
+		currentContainer.push(declaration);
+	}
+
+	@Override
+	public void exitDeclaration(ICSSParser.DeclarationContext ctx) {
+		super.exitDeclaration(ctx);
+	}
+
+	@Override
+	public void enterProperty(ICSSParser.PropertyContext ctx) {
+		ASTNode property = new PropertyName();
+		currentContainer.push(property);
+	}
+
+	@Override
+	public void exitProperty(ICSSParser.PropertyContext ctx) {
+		super.exitProperty(ctx);
+	}
+
+	@Override
+	public void enterVariableassignment(ICSSParser.VariableassignmentContext ctx) {
+		ASTNode assignment = new VariableAssignment();
+		currentContainer.push(assignment);
+	}
+
+	@Override
+	public void exitVariableassignment(ICSSParser.VariableassignmentContext ctx) {
+		super.exitVariableassignment(ctx);
+	}
+
+	@Override
+	public void enterIfclause(ICSSParser.IfclauseContext ctx) {
+		ASTNode clause = new IfClause();
+		currentContainer.push(clause);
+	}
+
+	@Override
+	public void exitIfclause(ICSSParser.IfclauseContext ctx) {
+		super.exitIfclause(ctx);
+	}
+
+	@Override
+	public void enterElseclause(ICSSParser.ElseclauseContext ctx) {
+		ASTNode clause = new ElseClause();
+		currentContainer.push(clause);
+	}
+
+	@Override
+	public void exitElseclause(ICSSParser.ElseclauseContext ctx) {
+		super.exitElseclause(ctx);
+	}
+
+	@Override
+	public void enterExpression(ICSSParser.ExpressionContext ctx) {
+		if (ctx.getChildCount() == 3) {
+			Operation operation;
+			switch (ctx.getChild(1).getText()) {
+				case "*":
+					operation = new MultiplyOperation();
+					break;
+				case "+":
+					operation = new AddOperation();
+					break;
+				default:
+					operation = new SubtractOperation();
+			}
+			currentContainer.push(operation);
+	}
+
+	@Override
+	public void exitExpression(ICSSParser.ExpressionContext ctx) {
+		super.exitExpression(ctx);
+	}
+
+	@Override
+	public void enterBoolliteral(ICSSParser.BoolliteralContext ctx) {
+		//ASTNode bool = new BoolLiteral();
+		//currentContainer.push(bool);
+	}
+
+	@Override
+	public void exitBoolliteral(ICSSParser.BoolliteralContext ctx) {
+		super.exitBoolliteral(ctx);
+	}
+
+	@Override
+	public void enterColorliteral(ICSSParser.ColorliteralContext ctx) {
+		super.enterColorliteral(ctx);
+	}
+
+	@Override
+	public void exitColorliteral(ICSSParser.ColorliteralContext ctx) {
+		super.exitColorliteral(ctx);
+	}
+
+	@Override
+	public void enterPercentageliteral(ICSSParser.PercentageliteralContext ctx) {
+		super.enterPercentageliteral(ctx);
+	}
+
+	@Override
+	public void exitPercentageliteral(ICSSParser.PercentageliteralContext ctx) {
+		super.exitPercentageliteral(ctx);
+	}
+
+	@Override
+	public void enterPixelliteral(ICSSParser.PixelliteralContext ctx) {
+		super.enterPixelliteral(ctx);
+	}
+
+	@Override
+	public void exitPixelliteral(ICSSParser.PixelliteralContext ctx) {
+		super.exitPixelliteral(ctx);
+	}
+
+	@Override
+	public void enterScalarliteral(ICSSParser.ScalarliteralContext ctx) {
+		super.enterScalarliteral(ctx);
+	}
+
+	@Override
+	public void exitScalarliteral(ICSSParser.ScalarliteralContext ctx) {
+		super.exitScalarliteral(ctx);
+	}
+
+	@Override
+	public void enterVariable(ICSSParser.VariableContext ctx) {
+		ASTNode var = new VariableReference();
+		currentContainer.push(var);
+	}
+
+	@Override
+	public void exitVariable(ICSSParser.VariableContext ctx) {
+		super.exitVariable(ctx);
+	}
+
+	@Override
+	public void enterLiteral(ICSSParser.LiteralContext ctx) {
+		super.enterLiteral(ctx);
+	}
+
+	@Override
+	public void exitLiteral(ICSSParser.LiteralContext ctx) {
+		super.exitLiteral(ctx);
+	}
+
+	@Override
+	public void enterClassselector(ICSSParser.ClassselectorContext ctx) {
+		super.enterClassselector(ctx);
+	}
+
+	@Override
+	public void exitClassselector(ICSSParser.ClassselectorContext ctx) {
+		super.exitClassselector(ctx);
+	}
+
+	@Override
+	public void enterTagselector(ICSSParser.TagselectorContext ctx) {
+		super.enterTagselector(ctx);
+	}
+
+	@Override
+	public void exitTagselector(ICSSParser.TagselectorContext ctx) {
+		super.exitTagselector(ctx);
+	}
+
+	@Override
+	public void enterIdselector(ICSSParser.IdselectorContext ctx) {
+		super.enterIdselector(ctx);
+	}
+
+	@Override
+	public void exitIdselector(ICSSParser.IdselectorContext ctx) {
+		super.exitIdselector(ctx);
+	}
+
+	@Override
+	public void enterSelector(ICSSParser.SelectorContext ctx) {
+		super.enterSelector(ctx);
+	}
+
+	@Override
+	public void exitSelector(ICSSParser.SelectorContext ctx) {
+		super.exitSelector(ctx);
+	}
+
+	@Override
+	public void enterBody(ICSSParser.BodyContext ctx) {
+		super.enterBody(ctx);
+	}
+
+	@Override
+	public void exitBody(ICSSParser.BodyContext ctx) {
+		super.exitBody(ctx);
+	}
+
+	@Override
+	public void enterEveryRule(ParserRuleContext ctx) {
+		super.enterEveryRule(ctx);
+	}
+
+	@Override
+	public void exitEveryRule(ParserRuleContext ctx) {
+		super.exitEveryRule(ctx);
+	}
+
+	@Override
+	public void visitTerminal(TerminalNode node) {
+		super.visitTerminal(node);
+	}
+
+	@Override
+	public void visitErrorNode(ErrorNode node) {
+		super.visitErrorNode(node);
+	}
 }
