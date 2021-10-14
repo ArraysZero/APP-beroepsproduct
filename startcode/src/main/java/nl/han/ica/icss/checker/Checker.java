ab7e9b8cadc363b4152b95cf41741b0e193aaec4
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-10-15 01:26:14 +0200

    de checker doet het dan eindelijk

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -25,13 +25,10 @@ public class Checker {
     private void checkNode(ASTNode node, int depth){
         //System.out.println("check node");
         if(node instanceof Declaration){
-            //System.out.println("declaration");
             checkDeclaration((Declaration) node);
         } else if (node instanceof Operation){
-            //System.out.println("operation");
             checkOperation((Operation) node);
         } else if (node instanceof IfClause){
-            //System.out.println("if clause");
             checkIfClause((IfClause) node);
         } else if (node instanceof VariableAssignment){
             variableTypes.getFirst().put(((VariableAssignment) node).name.name, ((VariableAssignment) node).expression);
@@ -67,30 +64,57 @@ public class Checker {
         }
     }
 
+    private Literal checkVarType(VariableReference reference){
+        Expression expression = null;
+
+        for (int i = 0; i < variableTypes.getSize(); i++){
+            if (variableTypes.get(i).get(reference.name) != null){
+                expression = variableTypes.get(i).get(reference.name);
+            }
+        }
+
+        if (expression instanceof Literal){
+            return (Literal) expression;
+        } else if (expression instanceof Operation){
+            if (((Operation) expression).lhs instanceof Literal){
+                return (Literal) ((Operation) expression).lhs;
+            } else if (((Operation) expression).rhs instanceof Literal){
+                return (Literal) ((Operation) expression).rhs;
+            }
+        }
+
+        return null;
+    }
+
     private void checkDeclaration(Declaration declaration){
-        if (declaration.expression instanceof VariableReference || declaration.expression instanceof Operation){
-            return;
+        Expression value;
+        if (declaration.expression instanceof VariableReference){
+            value = checkVarType((VariableReference) declaration.expression);
+        } else {
+            value = declaration.expression;
         }
 
         switch (declaration.property.name){
             case "color":
                 System.out.println("check de color");
-                if(!(declaration.expression instanceof ColorLiteral)){
+                if(!(value instanceof ColorLiteral)){
                     declaration.setError("color value can only be color literal");
                 }
                 break;
             case "background-color":
-                if(!(declaration.expression instanceof ColorLiteral)){
+                if(!(value instanceof ColorLiteral)){
                     declaration.setError("background-color value can only be color literal");
                 }
                 break;
             case "width":
-                if (!(declaration.expression instanceof PixelLiteral || declaration.expression instanceof PercentageLiteral)){
+                //operation is also allowed because thats what u get from an operation
+                if (!(value instanceof PixelLiteral || value instanceof PercentageLiteral || value instanceof Operation)){
                     declaration.setError("width must have pixel or percentagevalue");
                 }
                 break;
             case "height":
-                if (!(declaration.expression instanceof PixelLiteral || declaration.expression instanceof PercentageLiteral)){
+                //operation is also allowed because thats what u get from an operation
+                if (!(value instanceof PixelLiteral || value instanceof PercentageLiteral || value instanceof Operation)){
                     declaration.setError("height must have pixel or percentagevalue");
                 }
                 break;
