9a83798c03c439a73eced4f6d88df7be50d02038
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-11-20 15:20:59 +0100

    we gaan ff tests toevoegen

---
+++ startcode/src/test/java/compiler/allTests.java
@@ -0,0 +1,13 @@
+package compiler;
+
+import nl.han.ica.icss.Pipeline;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class allTests {
+}
